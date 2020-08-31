8a7e7ae70b5b0a9767e591e88452306c184786d3
Parag Adhau <adhauparag64@gmail.com>
2020-09-01 00:52:41 +0530

    Refactored :All the three to one generic method to find maximum

--- src/main/java/com/bridgelabz/generics/FindMax.java
+++ src/main/java/com/bridgelabz/generics/FindMax.java
@@ -1,54 +1,22 @@
 package com.bridgelabz.generics;
 
 public class FindMax {
-    public Integer findMaxValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
-        Integer max = firstNumber;
-        if (secondNumber.compareTo(max) > 0) {
-            max = secondNumber;
+    /*
+     * Generic Method to Find the Max Value of passed Arguments
+     **/
+    public <E extends Comparable> E findMaxValue(E firstValue, E secondValue, E thirdValue){
+        E max = firstValue;
+        if (secondValue.compareTo(max) > 0) {
+            max = secondValue;
         }
-        if (thirdNumber.compareTo(max) > 0) {
-            max = thirdNumber;
-        }
-        printMax(max);
-        return max;
-    }
-
-    public Float findMaxValue(Float firstNumber, Float secondNumber, Float thirdNumber) {
-        Float max = firstNumber;
-        if (secondNumber.compareTo(max) > 0) {
-            max = secondNumber;
-        }
-        if (thirdNumber.compareTo(max) > 0) {
-            max = thirdNumber;
+        if (thirdValue.compareTo(max) > 0) {
+            max = thirdValue;
         }
         printMax(max);
         return max;
     }
 
-    public String findMaxValue(String firstLetter, String secondLetter, String thirdLetter) {
-        int max = firstLetter.length();
-        String letter = firstLetter;
-        if (secondLetter.length() > max) {
-            max = secondLetter.length();
-            letter = secondLetter;
-        }
-        if (thirdLetter.length() > max) {
-            max = thirdLetter.length();
-            letter = thirdLetter;
-        }
-        printMax(letter);
-        return letter;
-    }
-
-    private void printMax(Integer max) {
-        System.out.println("Largest Integer is = " + max);
-    }
-
-    private void printMax(Float max) {
-        System.out.println("Largest Float is = " + max);
-    }
-
-    private void printMax(String letter) {
-        System.out.println("Largest Letter is = " + letter);
+    public <E> void printMax(E max){
+        System.out.println("Max Value = "+max);
     }
 }
